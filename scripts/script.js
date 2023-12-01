9079f58334dbf17d36641c079b513a33256be069
kayane <96435724+kayaneb@users.noreply.github.com>
2023-12-01 12:17:07 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
-console.log("hi"):
+console.log("hi");
+
+
+
+
+var openKnop = document.querySelector('#button');
+	// var sluitenKnop = document.querySelector('#button');
+	var menu = document.querySelector('#menuitems');
+
+
+function openen(){
+  console.log('open')
+	menu.classList.toggle('open');
+}
+
+
+//eventlisteners
+
+	openKnop.addEventListener('click', openen);
+
+
+// bron: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar //
+
